80d00b013e0393579a5da54af510599d6822af86
Kieran <kieran.perkinton@gmail.com>
2022-10-29 12:53:18 +0100

    ✨CarterBot v1.2
    - CarterBot now removes the bulky downvote buttons on it's messages after 15s!
    
    - CarterBot's AI code is now all contained within the messageCreate.js file. Making changes and adding features couldn't be easier!

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -17,40 +17,6 @@ module.exports = {
                 await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
             }
             console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
-        
-// ===================================================
-//                      BUTTONS
-// ===================================================
-
-        } else if (interaction.isButton()) {
-            if (interaction.customId.startsWith('message_downvote_')) {
-                const tid = await interaction.customId.replace('message_downvote_', '').split('_'); // Gets response TID from end of button ID
-
-                await fetch("https://api.carterapi.com/v0/downvote", { // Sends downvote request to CarterAPI
-                    method: "POST",
-                    headers: {
-                        Accept: "application/json",
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        api_key: config.carterkey,
-                        tid: tid,
-                    }),
-                });
-
-                const row1 = new ActionRowBuilder() // Downvote button builder
-                .addComponents(
-                    new ButtonBuilder()
-                    .setCustomId('message_downvote' + Math.random() * 1000)
-                    .setLabel('Downvote')
-                    .setEmoji('⬇️')
-                    .setStyle(ButtonStyle.Secondary)
-                    .setDisabled(true),
-                );
-                
-                await interaction.update({ components: [row1] });
-                await interaction.channel.send({ content: '`Message has been downvoted!\`', ephemeral: true }) // Sends response to channel
-            }
         }
     },
 };
--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require('discord.js');
 const config = require('../config.json');
 
 module.exports = {
@@ -11,6 +11,7 @@ module.exports = {
 		if(message.channel.isDMBased() || config.channelmode === "on" && message.channel.id === config.channelid) { // Checks if the message comes from a DM, or if (when the config is on) it came from a specific channel
 			try {
 				await message.channel.sendTyping()
+
 				await fetch("https://api.carterapi.com/v0/chat", { // Fetches the response from CarterAPI
 					method: 'POST',
 					headers: {
@@ -24,17 +25,49 @@ module.exports = {
 					})
 				}).then(res => res.json())
 				.then(async data => {
+
 					const row1 = new ActionRowBuilder() // Downvote button builder
 					.addComponents(
 						new ButtonBuilder()
-							.setCustomId('message_downvote_' + data.tid)
+							.setCustomId('message_downvote')
 							.setLabel('Downvote')
 							.setEmoji('⬇️')
 							.setStyle(ButtonStyle.Secondary),
 					);
-					await message.channel.send({ content: `${data.output.text}`, components: [row1] }) // Sends response to channel
-					console.log('Message has been sent')
-				})
+					
+					const msg = await message.channel.send({ content: `${data.output.text}`, components: [row1] }) // Sends response to channel
+
+					const collector = msg.createMessageComponentCollector({ componentType: ComponentType.Button, time: 15000 });
+					collector.on('collect', async i => {
+						await fetch("https://api.carterapi.com/v0/downvote", { // Sends downvote request to CarterAPI
+							method: "POST",
+							headers: {
+								Accept: "application/json",
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify({
+								api_key: config.carterkey,
+								tid: data.tid,
+							}),
+						});
+
+						const row1 = new ActionRowBuilder() // Downvote button builder
+							.addComponents(
+								new ButtonBuilder()
+								.setCustomId('message_downvote')
+								.setLabel('Downvote')
+								.setEmoji('⬇️')
+								.setStyle(ButtonStyle.Secondary)
+								.setDisabled(true),
+							);	
+						await i.update({ components: [row1] });
+					});
+
+					await collector.on('end', collected => {
+						msg.edit({ components: [] });
+					});
+				});
+
 			} catch(error) {
 				console.log(error)
 			}
