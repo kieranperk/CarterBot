c7dca2b27aa7c2cf1d3a067c1e314ca26cbb1048
Kieran <kieran.perkinton@gmail.com>
2023-04-06 12:21:11 +0100

    🔧 Update config.example.json

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -7,7 +7,7 @@ module.exports = {
 	async execute(message) {	
 		if(message.author.id === config.clientId) return; // Returns if message is itself
 
-		if(message.channel.isDMBased() || config.channelmode === "on" && message.channel.id === config.channelid) { // Checks if the message comes from a DM, or if (when the config is on) it came from a specific channel
+		if(message.channel.isDMBased() || config.channelmode.toLowerCase() === "on" && message.channel.id === config.channelid) { // Checks if the message comes from a DM, or if (when the config is on) it came from a specific channel
 			try {
 				await message.channel.sendTyping()
 
